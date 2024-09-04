e32062e423cafda18f371c4777c11e18756a807f
shin-minhyuk <afs_style@naver.com>
2024-09-04 13:29:41 +0900

    🐛fix: 환경변수를 노출하던 커밋 삭제 및 리베이스

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { store } from './RTK/store.jsx'
+import { store } from './store/store.jsx'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
